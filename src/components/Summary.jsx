334447f0c9eff6a142b002681931b908c32fa76c
TerezL <terez14@seznam.cz>
2026-04-03 15:56:33 +0200

    pics

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,7 +3,7 @@ function Summary({ result }) {
     <div className={`summary-item w-[90%] rounded-xl my-3 mx-auto ${result.category.toLowerCase()}`}>
       <div className='flex justify-between items-center m-auto p-5 gap-4'>
         <div className="flex items-center gap-2">
-          <img src={result.icon} alt={result.category} className="profile-image" />
+          <img src={`${import.meta.env.BASE_URL}${result.icon}`} alt={result.category} className="profile-image" />
           <h2>{result.category}</h2>
         </div>
         <p className="text-black font-bold text-xl">{result.score}<span className="text-gray-400 font-normal text-base"> / 100</span></p>
